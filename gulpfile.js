d929cfd1031b78977a43ae412fc43d26287bf1f6
Ieva Jaksaityte <ieva.jaksaityte@gmail.com>
2018-09-08 22:31:52 +0300

    install gulp, top level tasks

---
+++ gulpfile.js
@@ -0,0 +1,11 @@
+const gulp = require('gulp');
+//const sass = require('gulp-sass');
+
+/*
+    TOP LEVEL FUNCTIONS
+
+    gulp.task - define tasks
+    gulp.src - point to files to use
+    gulp.dest - point to the folder to output
+    gulp.watch - watch files and folder for changes
+*/
